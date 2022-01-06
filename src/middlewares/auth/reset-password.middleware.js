41892eade8f105e6ae20874f82ad6b7c6e0ccb66
Doan Tran <57784958+trdoan@users.noreply.github.com>
2022-01-07 06:46:32 +0700

    test

--- src/middlewares/auth/reset-password.middleware.js
+++ src/middlewares/auth/reset-password.middleware.js
@@ -3,25 +3,26 @@ const bcrypt = require("bcryptjs");
 const { ERROR_MESSAGE } = require("../../../config");
 const setPasswordDefault = async (req, res, next) => {
   try {
-    const { email } = req.body;
-    const passwordDefault = (Math.random() + 1).toString(36).substring(2);
-    const userDetail = await User.findOne({
-      where: {
-        email,
-      },
-    });
-    if (userDetail) {
-      const salt = bcrypt.genSaltSync(10);
-      const hashPassword = bcrypt.hashSync(passwordDefault, salt);
-      userDetail.password = hashPassword;
-      userDetail.save();
-      req.userInfo = { ...userDetail, newPassword: passwordDefault };
-      next();
-    } else {
-      res.status(404).send({
-        messages: "Email không chính xác",
-      });
-    }
+    // const { email } = req.body;
+    // const passwordDefault = (Math.random() + 1).toString(36).substring(2);
+    // const userDetail = await User.findOne({
+    //   where: {
+    //     email,
+    //   },
+    // });
+    // if (userDetail) {
+    //   const salt = bcrypt.genSaltSync(10);
+    //   const hashPassword = bcrypt.hashSync(passwordDefault, salt);
+    //   userDetail.password = hashPassword;
+    //   userDetail.save();
+    //   req.userInfo = { ...userDetail, newPassword: passwordDefault };
+    //   next();
+    // } else {
+    //   res.status(404).send({
+    //     messages: "Email không chính xác",
+    //   });
+    // }
+    res.send("SUCCESS")
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: ERROR_MESSAGE });
