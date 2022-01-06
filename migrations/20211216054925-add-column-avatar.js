1ceb8af16533d70990674107e94542bf1fd9731a
trdoan <tranduongdoan0922@gmail.com>
2022-01-06 12:13:51 +0700

    test connect db

--- migrations/20211216054925-add-column-avatar.js
+++ migrations/20211216054925-add-column-avatar.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn("users", "avatar", {
+    await queryInterface.addColumn("Users", "avatar", {
       type: Sequelize.TEXT,
       defaultValue:
         "https://res.cloudinary.com/depatula1/image/upload/v1639634403/guwf0wtqyfpgvigv1m0f.jpg",
