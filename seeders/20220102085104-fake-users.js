2506ec2a649faa03a8af1682f90c71a10c6d1eac
trdoan <tranduongdoan0922@gmail.com>
2022-02-09 20:18:21 +0700

    update seeed user

--- seeders/20220102085104-fake-users.js
+++ seeders/20220102085104-fake-users.js
@@ -11,8 +11,11 @@ module.exports = {
           id: 1000,
           hoTen: "admin",
           email: "admin@gmail.com",
-          matKhau: "",
+          matKhau:
+            "$2y$10$0bo.a7cQX0ba820EpytWxuublc.tdZYwBbhkGBsGyA3DJV5kiLkMC",
           soDT: "00",
+          avatar:
+            "https://res.cloudinary.com/depatula1/image/upload/v1644412506/avatar/sjewjmsqme2m7ngtlzue.png",
           nhomQuyen: ROLE_USER_ADMIN,
           createdAt: "2022-02-02",
           updatedAt: "2022-02-02",
@@ -21,8 +24,11 @@ module.exports = {
           id: 1001,
           hoTen: "client",
           email: "client@gmail.com",
-          matKhau: "",
+          matKhau:
+            "$2y$10$0bo.a7cQX0ba820EpytWxuublc.tdZYwBbhkGBsGyA3DJV5kiLkMC",
           soDT: "01",
+          avatar:
+            "https://res.cloudinary.com/depatula1/image/upload/v1644412546/avatar/fspbdxlw8oq4pts1zjzw.png",
           nhomQuyen: ROLE_USER_CLIENT,
           createdAt: "2022-02-02",
           updatedAt: "2022-02-02",
