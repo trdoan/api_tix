1ac9d04bf5b928d1f7146a64582e5b96acd617fe
trdoan <tranduongdoan0922@gmail.com>
2022-02-14 14:45:55 +0700

    change env

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const PORT = process.env.PORT;
+const APP_PORT = process.env.APP_PORT;
 const APP_SECRET_KEY = process.env.APP_SECRET_KEY;
 const SAVE_LOCAL = process.env.SAVE_LOCAL;
 const ROLE_USER_CLIENT = process.env.ROLE_USER_CLIENT;
@@ -7,7 +7,7 @@ const ROLE_USER_ADMIN = process.env.ROLE_USER_ADMIN;
 const AVATAR_USER_DEFAUT = process.env.AVATAR_USER_DEFAUT;
 const ERROR_MESSAGE = process.env.ERROR_MESSAGE;
 module.exports = {
-  PORT,
+  APP_PORT,
   APP_SECRET_KEY,
   SAVE_LOCAL,
   ERROR_MESSAGE,
--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 const express = require("express");
 const { rootRouter } = require("./src/routers/root.router");
 const cors = require("cors");
-const { PORT, SAVE_LOCAL } = require("./config");
+const { APP_PORT, SAVE_LOCAL } = require("./config");
 const path = require("path");
 const app = express();
 const http = require("http");
+// fix cors issues localhost
 app.use(cors());
+
 // format JSON request
 app.use(express.json());
 
 if (SAVE_LOCAL == "true") {
-  // setup static file
   const pathPublicDir = path.join(__dirname, "./public");
-  // http://localhost:3030/public
   app.use(express.static(pathPublicDir));
 }
 
-// http://localhost:3030/
-app.get("/", (req, res) => {
-  res.send("Welcome to my API clone-tix.vn by doandev");
-});
-setInterval(function () {
-  console.log("hack herokuapp");
+setInterval(() => {
   http.get("http://api-react-movie.herokuapp.com");
-}, 300000 * 4);
+}, 300000);
 
 app.use("/api/v1", rootRouter);
 
-app.listen(PORT, () => {
-  console.log("listening on PORT", PORT);
+app.listen(APP_PORT, () => {
+  console.log("listening on PORT", APP_PORT);
 });
