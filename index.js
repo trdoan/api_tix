8a470817503fe68bfdba287d7dbc2ed0796fb56f
trdoan <tranduongdoan0922@gmail.com>
2022-03-09 21:56:06 +0700

    test cookies

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const http = require("http");
 // fix cors issues localhost
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({ credentials: true, origin: "https://react-movie-ruby.vercel.app/" })
+);
+
 app.use(cookieParser());
 // format JSON request
 app.use(express.json());
